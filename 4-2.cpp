0609a3ab7ac6bbb0607ddf41c29872d8a68c6b10
finalchild <me@finalchild.dev>
2022-12-04 20:29:24 +0900

    day 4

---
+++ 4-2.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <set>
+#include <vector>
+#include <numeric>
+#define repeat(i, n) for (decay_t<decltype(n)> i = 0; i < (n); i++)
+using namespace std;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    i64 cnt = 0;
+    while (cin.peek() != char_traits<char>::eof()) {
+        i32 a; cin >> a; cin.get(); i32 b; cin >> b; cin.get();
+        i32 c; cin >> c; cin.get(); i32 d; cin >> d;
+        if (b >= c && d >= a) cnt++;
+        cin.ignore();
+    }
+
+    cout << cnt << '\n';
+    return 0;
+}
